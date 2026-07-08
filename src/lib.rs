e3fab08479387914bf7978b016453e5d4e1125b9
loongsoldier <zlb@rustsbi.com>
2026-07-08 20:35:04 +0800

    feat: add software debounce, move active_edge into ButtonConfig, use derive_setters
    - Add 20ms default debounce with edge-trigger + level-confirm pattern
    - Extend trait bound to Wait + InputPin for level reading
    - Move active_edge into ButtonConfig, remove with_edge/with_config
    - Derive Setters on ButtonConfig for fluent config API
    - Bump version to 0.4.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![no_std]
 
+use derive_setters::Setters;
 use embassy_futures::select::{Either, select};
 use embassy_time::{Duration, Instant, Timer};
+use embedded_hal::digital::{ErrorType, InputPin};
 use embedded_hal_async::digital::Wait;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -16,10 +18,16 @@ pub enum ButtonEdge {
     Rising,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Setters)]
+#[setters(no_std)]
 pub struct ButtonConfig {
+    #[setters(into)]
     pub long_press: Duration,
+    #[setters(into)]
     pub click_window: Duration,
+    #[setters(into)]
+    pub debounce: Duration,
+    pub active_edge: ButtonEdge,
 }
 
 impl Default for ButtonConfig {
@@ -27,42 +35,23 @@ impl Default for ButtonConfig {
         Self {
             long_press: Duration::from_millis(600),
             click_window: Duration::from_millis(200),
-        }
-    }
-}
-
-pub struct Button<P: Wait> {
-    pin: P,
-    config: ButtonConfig,
-    active_edge: ButtonEdge,
-}
-
-impl<P: Wait> Button<P> {
-    pub fn new(pin: P) -> Self {
-        Self {
-            pin,
-            config: ButtonConfig::default(),
+            debounce: Duration::from_millis(20),
             active_edge: ButtonEdge::Falling,
         }
     }
+}
 
-    pub fn with_edge(pin: P, edge: ButtonEdge) -> Self {
-        Self {
-            pin,
-            config: ButtonConfig::default(),
-            active_edge: edge,
-        }
+pub struct Button<P: Wait + InputPin> {
+    pin: P,
+    config: ButtonConfig,
+}
+
+impl<P: Wait + InputPin> Button<P> {
+    pub fn new(pin: P, config: ButtonConfig) -> Self {
+        Self { pin, config }
     }
 
-    pub fn with_config(pin: P, config: ButtonConfig, edge: ButtonEdge) -> Self {
-        Self {
-            pin,
-            config,
-            active_edge: edge,
-        }
-    }
-
-    pub async fn next_event(&mut self) -> Result<ButtonEvent, P::Error> {
+    pub async fn next_event(&mut self) -> Result<ButtonEvent, <P as ErrorType>::Error> {
         let long_press = self.config.long_press;
         let click_window = self.config.click_window;
 
@@ -101,17 +90,38 @@ impl<P: Wait> Button<P> {
         }
     }
 
-    async fn wait_for_press(&mut self) -> Result<(), P::Error> {
-        match self.active_edge {
-            ButtonEdge::Falling => self.pin.wait_for_falling_edge().await,
-            ButtonEdge::Rising => self.pin.wait_for_rising_edge().await,
+    fn is_pressed(&mut self) -> Result<bool, <P as ErrorType>::Error> {
+        Ok(match self.config.active_edge {
+            ButtonEdge::Falling => self.pin.is_low()?,
+            ButtonEdge::Rising => self.pin.is_high()?,
+        })
+    }
+
+    async fn wait_for_press(&mut self) -> Result<(), <P as ErrorType>::Error> {
+        let debounce = self.config.debounce;
+        loop {
+            match self.config.active_edge {
+                ButtonEdge::Falling => self.pin.wait_for_falling_edge().await?,
+                ButtonEdge::Rising => self.pin.wait_for_rising_edge().await?,
+            }
+            Timer::after(debounce).await;
+            if self.is_pressed()? {
+                return Ok(());
+            }
         }
     }
 
-    async fn wait_for_release(&mut self) -> Result<(), P::Error> {
-        match self.active_edge {
-            ButtonEdge::Falling => self.pin.wait_for_rising_edge().await,
-            ButtonEdge::Rising => self.pin.wait_for_falling_edge().await,
+    async fn wait_for_release(&mut self) -> Result<(), <P as ErrorType>::Error> {
+        let debounce = self.config.debounce;
+        loop {
+            match self.config.active_edge {
+                ButtonEdge::Falling => self.pin.wait_for_rising_edge().await?,
+                ButtonEdge::Rising => self.pin.wait_for_falling_edge().await?,
+            }
+            Timer::after(debounce).await;
+            if !self.is_pressed()? {
+                return Ok(());
+            }
         }
     }
 }
